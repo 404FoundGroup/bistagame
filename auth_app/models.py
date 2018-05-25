f1f00206a4f56da283e4bf3a8d52453cc4046a51
Aria Moradi <aria.moradi007@gmail.com>
2018-05-25 19:49:39 +0430

    fixed the problems with previous data Models

--- auth_app/models.py
+++ auth_app/models.py
@@ -5,23 +5,27 @@ from django.db import models
 # TODO: recode the User class to use the django auth system
 class User(models.Model):
     email = models.EmailField()
-    username = models.CharField()
-    password = models.CharField()
+    username = models.CharField(max_length=32)
+    password = models.CharField(max_length=64)
+
+class Medal(models.Model):
+    name = models.CharField(max_length=64)
 
 class UserData(models.Model):
-    medalList = models.CharField()
-    #gameList = models.ForeignKey() -> this is represented in the game table
-    frieds = models.ForeignKey() # TODO: fix this
-    blocked = models.CharField()
+    medal = models.ManyToManyField(Medal)
     totalscore = models.IntegerField()
-    #gamesPlayed = models.ForeignKey() -> this is represented in the game table
     winCount = models.IntegerField()
     loseCount = models.IntegerField()
     leaveCount = models.IntegerField()
 
+class Friendship(models.Model):
+    user = models.ForeignKey(User)
+    friend = models.ForeignKey(User)
+    blocked = models.BooleanField() # relation is in blocked or firend state
+
 class Game(models.Model):
-    playerAsk = models.ForeignKey()
-    playerAsn = models.ForeignKey()
-    log = models.CharField()
-    soreAsk = models.IntegerField()
-    soreAns = models.IntegerField()
+    playerAsk = models.ForeignKey(User)
+    playerAsn = models.ForeignKey(User)
+    log = models.TextField()
+    scoreAsk = models.IntegerField()
+    scoreAns = models.IntegerField()
