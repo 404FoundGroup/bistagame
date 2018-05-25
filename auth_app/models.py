0c9e8d62f50dd30ad49ce7cd019fc9d1e9f63578
Aria Moradi <aria.moradi007@gmail.com>
2018-05-26 00:49:33 +0430

    finilized dataModels and wrote test for models in auth_app

--- auth_app/models.py
+++ auth_app/models.py
@@ -8,10 +8,17 @@ class User(models.Model):
     username = models.CharField(max_length=32)
     password = models.CharField(max_length=64)
 
+    def get_friends(self):
+        return Friendship.objects.filter(creator=self.username)
+
+    def __str__(self):
+        return self.username
+
 class Medal(models.Model):
     name = models.CharField(max_length=64)
 
 class UserData(models.Model):
+    user = models.OneToOneField(User,models.CASCADE)
     medal = models.ManyToManyField(Medal)
     totalscore = models.IntegerField()
     winCount = models.IntegerField()
@@ -19,13 +26,19 @@ class UserData(models.Model):
     leaveCount = models.IntegerField()
 
 class Friendship(models.Model):
-    user = models.ForeignKey(User)
-    friend = models.ForeignKey(User)
-    blocked = models.BooleanField() # relation is in blocked or firend state
+    creator = models.ForeignKey(User,models.CASCADE,related_name="friends_set")
+    friend = models.ForeignKey(User,models.CASCADE)
+    blocked = models.BooleanField(default=False)# relation is in blocked or firend state
 
-class Game(models.Model):
-    playerAsk = models.ForeignKey(User)
-    playerAsn = models.ForeignKey(User)
+def get_deleted_user():
+    """
+        get a refrence to a deleted user
+    """
+    return User.objects.get_or_create(username='deleted user')[0]
+
+class GameLog(models.Model):
+    playerAsk = models.ForeignKey(User,models.SET(get_deleted_user),related_name='gamelog_asker_set')
+    playerAsn = models.ForeignKey(User,models.SET(get_deleted_user),related_name='gamelog_answerer_set')
     log = models.TextField()
     scoreAsk = models.IntegerField()
     scoreAns = models.IntegerField()
---
+++ auth_tester/setup_django.py
@@ -0,0 +1,4 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bistgame.settings")
+import django
+django.setup()
