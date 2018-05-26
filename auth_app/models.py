d22062805e85ccdbde690dd7718f4721c852f194
sajjad.rezaee.001@gmail.com <sajjad.rezaee.001@gmail.com>
2018-05-26 15:44:50 +0430

    deleted delet_user function in whole of the app

--- auth_app/models.py
+++ auth_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from enum import Enum
 
+
 # Create your models here.
 
 # TODO: recode the User class to use the django auth system
@@ -8,28 +9,30 @@ class User(models.Model):
     email = models.EmailField()
     username = models.CharField(max_length=32)
     password = models.CharField(max_length=64)
-
     def get_friends(self):
         return Friendship.objects.filter(creator=self.username)
 
     def __str__(self):
         return self.username
 
+
 class Medal(models.Model):
     name = models.CharField(max_length=64)
 
+
 class UserData(models.Model):
-    user = models.OneToOneField(User,models.CASCADE)
+    user = models.OneToOneField(User, models.CASCADE)
     medal = models.ManyToManyField(Medal)
     totalscore = models.IntegerField()
     winCount = models.IntegerField()
     loseCount = models.IntegerField()
     leaveCount = models.IntegerField()
 
+
 class Friendship(models.Model):
-    creator = models.ForeignKey(User,models.CASCADE,related_name="friends_set")
-    friend = models.ForeignKey(User,models.CASCADE)
-    blocked = models.BooleanField(default=False)# relation is in blocked or firend state
+    creator = models.ForeignKey(User, models.CASCADE, related_name="friends_set")
+    friend = models.ForeignKey(User, models.CASCADE)
+    blocked = models.BooleanField(default=False)  # relation is in blocked or firend state
 
 
 class gameOutcome(Enum):
@@ -37,15 +40,11 @@ class gameOutcome(Enum):
 
 
 class GameLog(models.Model):
-    def get_deleted_user():
-        """
-            get a refrence to a deleted user
-        """
-        return User.objects.get_or_create(username='deleted user')[0]
 
-    playerAsk = models.ForeignKey(User,models.SET(get_deleted_user),related_name='gamelog_asker_set')
-    playerAsn = models.ForeignKey(User,models.SET(get_deleted_user),related_name='gamelog_answerer_set')
+
+    playerAsk = models.ForeignKey(User, models.CASCADE, related_name='gamelog_asker_set')
+    playerAns = models.ForeignKey(User, models.CASCADE, related_name='gamelog_answerer_set')
     log = models.TextField()
     scoreAsk = models.IntegerField()
-    scoreAns = models.IntegerField()
+    scoreAns = models.IntegerField() #//////////////////// edit
     outcome = models.IntegerField()
